78fc8fef9aaa3acd76279bf6020ce3ae92f696df
AlloVince <allo.vince@gmail.com>
2015-11-21 00:07:51 +0800

    fix warnings

--- main.py
+++ main.py
@@ -1,74 +1,81 @@
 # -*- coding: utf8 -*-
+import traceback
 
 import settings
 from pathlib2 import *
 import re
 import requests
-from pyquery import PyQuery
 import json
 import os
 import logging
-import traceback
 
-source_dir = 'Z:\Issue'
-target_dir = 'Z:\dist'
+# source_dir = 'Z:\Issue'
+source_dir = '/Volumes/TB/TDDOWNLOAD'
+# target_dir = 'Z:\dist'
+target_dir = '/Volumes/TB/dist'
+path_encoding = 'utf8'
 rename_format = '%actor% - %title% [%bango%]'
+
+
 # Debug:
-# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.ERROR)
+
 
 def print_path(file):
-    return file.as_posix().decode('gbk').encode('utf8')
+    return file.as_posix().decode(path_encoding).encode('utf8')
 
-def find_bango_in_file(file):
-    logging.warning("--------------------------------------------------------------------------------------")
-    logging.warning("Processing: %s" % print_path(file))
+
+def find_banngo_in_file(file):
+    logging.info("--------------------------------------------------------------------------------------")
+    logging.info("Processing: %s" % print_path(file))
     """
     :type file: PurePath
     """
-    filename = file.name.decode('gbk').encode('utf8')
+    filename = file.name.decode(path_encoding).encode('utf8')
     # print("processing %s", file.as_uri())
-    bango = re.search(r'([a-zA-Z]{2,6})-?(\d{2,5})', filename)
-    if bango:
-        logging.warning("Found bango: %s" % bango.group())
-        return bango.group()
+    banngo = re.search(r'([a-zA-Z]{2,6})-?(\d{2,5})', filename)
+    if banngo:
+        logging.warning("Found bango: %s" % banngo.group())
+        return banngo.group()
     else:
         logging.warning("Result: Not found bango, skiped")
         return None
 
 
-def get_movie_detail(bango):
+def get_movie_detail(banngo):
     # Try search for once
     search_url = "http://yinxing.com/v1/movies?q="
     search_result = requests.get(
-        search_url + bango, timeout=2)
-    logging.warning(
-        "Try search bango: %s" % search_url + bango)
+        search_url + banngo, timeout=2)
+    logging.info(
+        "Try search banngo: %s" % search_url + banngo)
 
     if search_result.status_code != 200:
         logging.warning("Result: not found in website by %s in %s" % (search_result.status_code, search_result.url))
         return None
-    search_result = json.loads(search_result.text)
-    # print search_result['items'][0]['link']
-    # print re.search("-/detail/=", search_result['items'][0]['link'])
-    if len(search_result['results']) < 1:
+    result = json.loads(search_result.text)
+
+    if len(result['results']) < 1:
         logging.warning("Result: not found %s in %s" % (search_result.status_code, search_result.url))
         return None
-    return search_result['results'][0]
+    return result['results'][0]
+
 
 def path_filter(name):
-    name = name.replace('/', '.')
-    # name = name.replace('・', '.')
+    name = name.replace('/', '.').strip()
+    name = name.replace(u'・', '.')
     return name
 
 
 def arrange_file(movie, file):
     maker = movie['maker']['name'] if movie['maker'] else 'unknown'
-    logging.warning("movie %s" % movie)
-    target_path = target_dir + '/' + path_filter(maker).encode("gbk")
-    logging.warning("Target path: %s" % print_path(Path(target_path)))
+    logging.info("Found movie as %s" % movie)
+    target_path = target_dir + '/' + path_filter(maker).encode(path_encoding)
+    logging.info("Target path: %s" % print_path(Path(target_path)))
     if Path(target_path).exists() is False:
         Path(target_path).mkdir(parents=True, mode=0o777)
-        logging.warning("Target path not exists and be created")
+        logging.warning("Target path %s not exists and be created" % target_path)
 
     casts = movie["casts"]
     casts_list = []
@@ -77,11 +84,12 @@ def arrange_file(movie, file):
     casts_list.reverse()
     casts = "" if len(casts_list) < 1 else ", ".join(casts_list) + " - "
     target_file = "%s%s [%s]" % (casts, path_filter(movie['title']), path_filter(movie['banngo']))
-    target_cover = target_path + "/" + (target_file + '.jpg').encode("gbk")
-    target_file = target_path + "/" + (target_file + file.suffix).encode("gbk")
+    target_cover = target_path + "/" + (target_file + '.jpg').encode(path_encoding)
+    target_file = target_path + "/" + (target_file + file.suffix).encode(path_encoding)
 
     if Path(target_cover).exists():
-        logging.error("Result: Target %s exists, skiped" % print_path(Path(target_file)))
+        logging.error("TODO: FILE %s target %s exists, skiped" % (file, print_path(Path(target_file))))
+        os.remove(file.as_posix())
         return None
 
     if Path(target_cover).exists() is False:
@@ -139,7 +147,7 @@ def delete_empty_dir(file):
 
 
 def process(file):
-    bango = find_bango_in_file(file)
+    bango = find_banngo_in_file(file)
     if not bango:
         return None
     movie = get_movie_detail(bango)
@@ -160,5 +168,6 @@ for ext in ["avi", "mkv", "mp4"]:
         try:
             process(cfile)
         except:
-            # logging.warning("Result: Unkown exception happens for %s, detail:\n %s" % (print_path(cfile), traceback.print_exc()))
+            logging.warning(
+                "Result: Unknown exception happens for %s, detail:\n %s" % (print_path(cfile), traceback.print_exc()))
             continue
